cdbcf8bc1a163dda003ef0ba74f3e2b3f46bc02d
Dylan Kilkenny <dylankilkenny95@gmail.com>
2020-01-09 19:42:05 +0000

    added config and ability to update credentials to proxy

--- proxy/go.mod
+++ proxy/go.mod
@@ -3,6 +3,9 @@ module eirevpn/proxy
 go 1.12
 
 require (
+	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5
 	github.com/elazarl/goproxy v0.0.0-20190711103511-473e67f1d7d2
-	github.com/elazarl/goproxy/ext v0.0.0-20190711103511-473e67f1d7d2 // indirect
+	github.com/elazarl/goproxy/ext v0.0.0-20190711103511-473e67f1d7d2
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	gopkg.in/yaml.v2 v2.2.7
 )
--- proxy/main.go
+++ proxy/main.go
@@ -1,14 +1,59 @@
 package main
 
 import (
+	c "eirevpn/proxy/config"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/elazarl/goproxy"
+	"github.com/elazarl/goproxy/ext/auth"
 )
 
+var config c.Config
+
+type credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func main() {
+	config = c.GetConfig()
+	fmt.Println(config)
+	go startProxy()
+	startAPI()
+}
+
+func startAPI() {
+	http.HandleFunc("/update", func(w http.ResponseWriter, r *http.Request) {
+		d := json.NewDecoder(r.Body)
+		cred := &credentials{}
+		err := d.Decode(cred)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		config.App.ProxyUsername = cred.Username
+		config.App.ProxyPassword = cred.Password
+		fmt.Println(config)
+	})
+	fmt.Println("REST API Running")
+	http.ListenAndServe(":"+config.App.RestPort, nil)
+}
+
+func startProxy() {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
-	log.Fatal(http.ListenAndServe(":8080", proxy))
+	auth.ProxyBasic(proxy, "Auth", func(user, passwd string) bool {
+		fmt.Println(config)
+		if user == config.App.ProxyUsername && passwd == config.App.ProxyPassword {
+			fmt.Println("correct user pass")
+			return true
+		}
+		fmt.Println("wrong user pass")
+		return false
+	})
+	fmt.Println("Proxy Running")
+	log.Fatal(http.ListenAndServe(":"+config.App.ProxyPort, proxy))
 }
